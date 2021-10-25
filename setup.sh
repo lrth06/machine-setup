45a873398c5806510716ce00f442d4a7969b6aeb
lrth06 <lrth06@gmail.com>
2021-10-25 12:22:39 -0400

    correct Node install

--- setup.sh
+++ setup.sh
@@ -1,17 +1,25 @@
 !#/usr/bin/env bash
 echo "Setting up new development machine..."
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install zsh -y
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-sudo apt-get install git -y
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-source ~/.zshrc
+apt-get install zsh -y && chsh -s /bin/zsh
+apt-get install git -y
+apt-get install wget -y
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash && source ~/.nvm/nvm.sh
+wget -c https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
+apt-get install python3 -y
+apt-get install python3-pip -y
+
 nvm install --lts
 nvm use --lts
-wget -c https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
 source ~/.zshrc
-sudo apt-get install python3 -y
-sudo apt-get install python3-pip -y
+mkdir -p ~/github.com 
+mkdir -p ~/local $$ mkdir -p ~/local/node && mkdir -p ~/local/go
+mkdir -p ~/local/python
 
+echo "Install complete! Versions Installed:"
+nvm --version 
+npm --version
+go version
+python3 --version
+pip3 --version 
